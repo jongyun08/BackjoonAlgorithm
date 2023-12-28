eb3b7dcfa373263b7a79fee66fb02c4e1c3aaa8b
jongyun08 <142795548+jongyun08@users.noreply.github.com>
2023-12-28 14:07:33 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+unsigned long long A;
+unsigned long long B;
+unsigned long long C;
+
+int main()
+{
+    cin >> A;
+    cin >> B;
+    cin >> C;
+    cout << A + B + C << endl;
+    return 0;
+}
