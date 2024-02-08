a6e77f2715fc47f0c1a95ea122c49164affaf5c2
jongyun08 <142795548+jongyun08@users.noreply.github.com>
2024-02-08 18:19:31 +0900

    [Bronze V] Title: X보다 작은 수, Time: 420 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int N, X;
+	cin >> N >> X;
+	int* arr = (int*)malloc(sizeof(int) * N);
+	for (int i = 0; i < N; ++i)
+	{
+		cin >> arr[i];
+		if (arr[i] < X)
+		{
+			cout << arr[i] << " ";
+		}
+	}
+	cout << endl;
+
+	return 0;
+}
