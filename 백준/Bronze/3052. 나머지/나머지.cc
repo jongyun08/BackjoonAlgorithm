08c5df465d8229798b5d83b2c70176d21fad71f4
jongyun08 <142795548+jongyun08@users.noreply.github.com>
2024-02-09 19:17:04 +0900

    [Bronze II] Title: 나머지, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3052. 나머지/나머지.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int input;
+	int count = 0;
+	int arr[42] = { 0 };
+
+	for (int i = 0; i < 10; ++i)
+	{
+		cin >> input;
+		arr[input % 42] = 1;
+	}
+	
+	for (int i = 0; i < 42; ++i)
+	{
+		if (arr[i] == 1)
+		{
+			
+			count++;
+			
+		}
+	}
+	cout << count << endl;
+	return 0;
+}
